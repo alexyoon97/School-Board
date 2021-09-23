9447b61f7753f571033487b6519987c4b530e7da
Alex Yoon <43189680+alexyoon97@users.noreply.github.com>
2021-09-23 02:24:00 -0700

    make it better

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const { Pool ,Client } = require("pg");
 const pool = new Pool({
-  user: "postgres",
+  user: "zryigjdlrkdqlf",
   password: process.env.PG_PASS,
-  host: "localhost",
+  host: "ec2-54-145-110-118.compute-1.amazonaws.com",
   port: "5432",
-  database: "Dreamschool",
+  database: "dde30rpmi0tt2k",
 });
 
 module.exports = pool;
