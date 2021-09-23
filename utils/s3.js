abeebc279483cc1da3b35624c594fc3017775f93
Alex Yoon <43189680+alexyoon97@users.noreply.github.com>
2021-09-23 02:00:57 -0700

    make it better

--- utils/s3.js
+++ utils/s3.js
@@ -1,30 +1,30 @@
-require('dotenv').config()
-const fs = require('fs')
-const S3 = require('aws-sdk/clients/s3')
+require('dotenv').config();
+const fs = require('fs');
+const S3 = require('aws-sdk/clients/s3');
 
-const bucketName = process.env.Bucket
-const region = process.env.region
-const accessKeyId = process.env.AWSAccessKeyId
-const secretAccessKey = process.env.AWSSecretKey
+const bucketName = process.env.Bucket;
+const region = process.env.region;
+const accessKeyId = process.env.AWSAccessKeyId;
+const secretAccessKey = process.env.AWSSecretKey;
 
 const s3 = new S3({
   region,
   accessKeyId,
   secretAccessKey
-})
+});
 
 // uploads a file to s3
 function uploadFile(file) {
-  const fileStream = fs.createReadStream(file.path)
+  const fileStream = fs.createReadStream(file.path);
   const uploadParams = {
     Bucket: bucketName,
     Body: fileStream,
     Key: file.filename
   }
 
-  return s3.upload(uploadParams).promise()
+  return s3.upload(uploadParams).promise();
 }
-exports.uploadFile = uploadFile
+exports.uploadFile = uploadFile;
 
 
 // downloads a file from s3
@@ -34,6 +34,6 @@ function getFileStream(fileKey) {
     Bucket: bucketName
   }
 
-  return s3.getObject(downloadParams).createReadStream()
+  return s3.getObject(downloadParams).createReadStream();
 }
-exports.getFileStream = getFileStream
+exports.getFileStream = getFileStream;
